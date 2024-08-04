172ab934f34a70a07e98e18c057d3ef937c92aec
Bleak Grey <bleakgrey@e.email>
2024-08-04 20:21:26 +0400

    Add Howler sound service

---
+++ src/engine/audio/HowlerAudioPlayer.ts
@@ -0,0 +1,41 @@
+import { Sounds } from "@/Sounds"
+import { IAudioService } from "./IAudioService"
+import { Howl, Howler } from 'howler'
+
+export class SimpleAudioPlayer implements IAudioService {
+    private players = new Map<String, Howl>()
+    private persistentSounds = new Set<String>()
+
+    constructor() {
+        this.init()
+    }
+
+    public play(name: any) {
+        const player = this.players.get(String(name))
+
+        if (Howler.ctx.state === 'running' || this.persistentSounds.has(String(name))) {
+            player?.play()
+        }
+    }
+
+    public stop(name: any) {
+        const player = this.players.get(String(name))
+        player?.stop()
+    }
+
+    private init() {
+        for (const [key, value] of Object.entries(Sounds)) {
+            const player = new Howl({
+                autoplay: false,
+                preload: true,
+                ...value,
+            })
+
+            if (value.persist) {
+                this.persistentSounds.add(String(key))
+            }
+
+            this.players.set(String(key), player)
+        }
+    }
+}
---
+++ src/engine/audio/IAudioService.ts
@@ -0,0 +1,4 @@
+export interface IAudioService {
+    play(name: any): void;
+    stop(name: any): void;
+}
