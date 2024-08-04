28f21f0dc0c580991496d9b2d98506ca22c912cf
Bleak Grey <bleakgrey@e.email>
2024-08-04 20:24:25 +0400

    Add EventBus

---
+++ src/scenes/lava/Events.ts
@@ -0,0 +1,13 @@
+export default {
+    TOGGLE_DRAG_DROP: 'toggleDragDrop',
+    DRAG_START: 'dragStart',
+    DRAG_END: 'dragEnd',
+    DRAG_ACCEPT: 'dragAccept',
+
+    SPAWN_ONESHOT_EFFECT: 'showOneshotEffect',
+
+    SHOW_TUTORIAL_HINT: 'showTutorialHint',
+
+    CHARACTER_SET_SEQUENCE: 'charSetSeq',
+    CHARACTER_SEQUENCE_END: 'charSeqEnd',
+}
