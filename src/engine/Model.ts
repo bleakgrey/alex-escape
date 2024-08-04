c7be546a21ac9e2a0644e16d933f6f4b3e2f9345
Bleak Grey <bleakgrey@e.email>
2024-08-04 20:13:29 +0400

    Add Model

---
+++ src/engine/Model.ts
@@ -0,0 +1,33 @@
+import EventEmitter from "eventemitter3"
+
+export class Model extends EventEmitter {
+
+    private watch<T extends EventEmitter>(
+        obj: T,
+    ): T {
+        const proxy = new Proxy(obj as object, {
+            set(target: EventEmitter, prop, newValue, receiver) {
+                const result = Reflect.set(...arguments)
+
+                target.emit(`${String(prop)}`, newValue)
+                target.emit('**', prop)
+
+                return result
+            },
+        })
+
+        // Trigger to immediately sync the state
+        for (const [key, value] of Object.entries(obj)) {
+            obj.emit(`${String(key)}`, key, value)
+        }
+        obj.emit('**')
+
+        return proxy as T
+    }
+
+    constructor() {
+        super()
+        return this.watch(this)
+    }
+
+}
