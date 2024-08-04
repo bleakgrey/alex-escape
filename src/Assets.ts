7188f04eeb6c72b42c558a0b5050be48bc2ebd65
Bleak Grey <bleakgrey@e.email>
2024-08-04 20:22:11 +0400

    Use asset manifest

---
+++ src/Assets.ts
@@ -0,0 +1,79 @@
+import BG from '@/assets/images/bg.jpg?url'
+import FG from '@/assets/images/fg.png?url'
+import VOLCANO from '@/assets/images/volcano.png?url'
+
+import PRIDI from '@/assets/fonts/pridi/pridi.txt?inline'
+import PRIDI_PAGE_0 from '@/assets/fonts/pridi/pridi.png?url'
+
+import BUBBLE from '@/assets/images/bubble.png?url'
+import LOG from '@/assets/images/log.png?url'
+import WOOD from '@/assets/images/wood.png?url'
+import LAVA from '@/assets/images/lava.png?url'
+import LAVA_SHAPE from '@/assets/images/lavaShape.min.svg?raw'
+import HAND from '@/assets/images/hand.png?url'
+
+import LOGO from '@/assets/images/logo.png?url'
+import FAIL from '@/assets/images/fail.png?url'
+import BUTTON from '@/assets/images/button.png?url'
+
+import HINT_LOG from '@/assets/images/hint0.png?url'
+import HINT_WOOD from '@/assets/images/hint1.png?url'
+import HINT_ARROW from '@/assets/images/hint2.png?url'
+
+import BRIDGE_ATLAS from '@/assets/images/bridge/bridge.png?url'
+import BRIDGE_JSON from '@/assets/images/bridge/bridge.json?inline'
+import BRIDGE_FG from '@/assets/images/bridge/bridge_fg.png?url'
+
+import EXPLOSION_ATLAS from '@/assets/images/vfx/Explosion.png?url'
+import EXPLOSION_JSON from '@/assets/images/vfx/Explosion.json?inline'
+import SWIRL_ATLAS from '@/assets/images/vfx/Swirl.png?url'
+import SWIRL_JSON from '@/assets/images/vfx/Swirl.json?inline'
+
+import ALEX_RUNNING from '@/assets/images/alex/Alex_running.png?url'
+import ALEX_RUNNING_ATLAS from '@/assets/images/alex/Alex_running.json?inline'
+import ALEX_JUMPING from '@/assets/images/alex/Alex_jumping.png?url'
+import ALEX_JUMPING_ATLAS from '@/assets/images/alex/Alex_jumping.json?inline'
+import ALEX_THINKING from '@/assets/images/alex/Alex_think.png?url'
+import ALEX_THINKING_ATLAS from '@/assets/images/alex/Alex_think.json?inline'
+import ALEX_SHOCKED from '@/assets/images/alex/Alex_shocked.png?url'
+import ALEX_SHOCKED_ATLAS from '@/assets/images/alex/Alex_shocked.json?inline'
+
+export default {
+	BG,
+	FG,
+	VOLCANO,
+
+	LOG,
+	WOOD,
+	BUBBLE,
+	LAVA,
+	LAVA_SHAPE,
+	
+	HAND,
+	HINT_LOG,
+	HINT_WOOD,
+	HINT_ARROW,
+	LOGO,
+	FAIL,
+    BUTTON,
+
+	BRIDGE_ATLAS,
+	BRIDGE_JSON,
+	BRIDGE_FG,
+	EXPLOSION_ATLAS,
+	EXPLOSION_JSON,
+	SWIRL_ATLAS,
+	SWIRL_JSON,
+
+	ALEX_RUNNING,
+	ALEX_RUNNING_ATLAS,
+	ALEX_JUMPING,
+	ALEX_JUMPING_ATLAS,
+	ALEX_THINKING,
+	ALEX_THINKING_ATLAS,
+	ALEX_SHOCKED,
+	ALEX_SHOCKED_ATLAS,
+
+	PRIDI_PAGE_0,
+	PRIDI,
+}
